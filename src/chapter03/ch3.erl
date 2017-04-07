c98982faa6023ca5226e01f3a339ca11934097c2
ortex <diskbu@yandex.ru>
2017-04-08 00:00:58 +0300

    finished 3.5

--- src/chapter03/ch3.erl
+++ src/chapter03/ch3.erl
@@ -3,6 +3,7 @@
 -export([sum/1, sum/2]).
 -export([create/1, reverse_create/1]).
 -export([print_n/1, print_odd_n/1]).
+-export([filter/2, reverse/1, concatenate/1, flatten/1]).
 
 %% 3.1
 sum(0) -> 0;
@@ -41,4 +42,37 @@ print_odd_n(N) when N rem 2 == 1 ->
   io:format("Number:~p~n", [N]);
 print_odd_n(N) ->
   print_odd_n(N - 1).
+
+%% 3.5
+
+filter([], _) -> [];
+filter([H | T], Min) when H =< Min ->
+  [H | filter(T, Min)];
+filter([_ | T], Min) ->
+  filter(T, Min).
+
+reverse(L) -> reverse(L, []).
+
+reverse([], L) -> L;
+reverse([H | T], L2) -> reverse(T, [H | L2]).
+
+
+concatenate([]) -> [];
+concatenate([[] | T]) ->
+  concatenate(T);
+concatenate([[H | Hs] | T]) ->
+  [H | concatenate([Hs | T])].
+
+%%concatenate(L) -> concatenate(L, []).
+%%concatenate([], R) -> reverse(R);
+%%concatenate([[] | T], R) -> concatenate(T, R);
+%%concatenate([[H | Hs] | T], R) ->
+%%  concatenate([Hs | T], [H | R]).
+
+
+flatten([]) -> [];
+flatten([H | T]) when not(is_list(H)) ->
+  [H | flatten(T)];
+flatten([H | T]) ->
+  flatten(H) ++ flatten(T).
 
